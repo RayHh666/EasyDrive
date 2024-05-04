daa512e55f23ccb3a515997552fbf5761dd9a2b5
RayHh666 <53267956+rayhh666@users.noreply.github.com>
2024-05-05 01:01:06 +0800

    控制层：测试文件

---
+++ src/main/java/com/EasyDrive/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.EasyDrive.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public String test(){
+        return "测试 测试 测试";
+    }
+}
